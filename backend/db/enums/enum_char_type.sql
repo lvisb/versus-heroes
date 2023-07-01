087db4dd5b97a9ebaf4d4996fc709267e78c4bbe
Luis <luis.flashdev@gmail.com>
2023-07-01 11:18:07 -0300

    feat(backend.db): characters table

---
+++ backend/db/enums/enum_char_type.sql
@@ -0,0 +1,1 @@
+CREATE TYPE enum_char_type AS ENUM ('hero', 'villain');
---
+++ backend/db/tables/characters.table.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS characters (
+  char_id SERIAL PRIMARY KEY,
+  author_id UUID REFERENCES auth.users (id),
+  char_type enum_char_type NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  description TEXT NOT NULL,
+  attributes JSONB NOT NULL,
+  strenghts VARCHAR(100)[] DEFAULT '{}',
+  weaknesses VARCHAR(100)[] DEFAULT '{}',
+  related_heros INT[] DEFAULT '{}',
+  related_villains INT[] DEFAULT '{}',
+  views INT DEFAULT 0,
+  is_active BOOLEAN DEFAULT FALSE,
+  is_deleted BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE INDEX idx_author_id ON characters (author_id);
+CREATE INDEX idx_char_type ON characters (char_type);
+CREATE INDEX idx_name ON characters (name);
+CREATE INDEX idx_related_heros ON characters (related_heros);
+CREATE INDEX idx_related_villains ON characters (related_villains);
+CREATE INDEX idx_is_active ON characters (is_active);
+CREATE INDEX idx_is_deleted ON characters (is_deleted);
+CREATE INDEX idx_created_at ON characters (created_at);
+CREATE INDEX idx_updated_at ON characters (updated_at);
+
