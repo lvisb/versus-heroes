d23c135daada3a6050244643b259be3cbde5bac3
Luis <luis.flashdev@gmail.com>
2023-07-09 14:02:32 -0300

    refactor(backend.db): characters index name correction

--- backend/db/tables/characters.table.sql
+++ backend/db/tables/characters.table.sql
@@ -16,13 +16,13 @@ CREATE TABLE IF NOT EXISTS characters (
   updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE INDEX idx_author_id ON characters (author_id);
-CREATE INDEX idx_char_type ON characters (char_type);
-CREATE INDEX idx_name ON characters (name);
-CREATE INDEX idx_related_heros ON characters (related_heros);
-CREATE INDEX idx_related_villains ON characters (related_villains);
-CREATE INDEX idx_is_active ON characters (is_active);
-CREATE INDEX idx_is_deleted ON characters (is_deleted);
-CREATE INDEX idx_created_at ON characters (created_at);
-CREATE INDEX idx_updated_at ON characters (updated_at);
+CREATE INDEX idx_characters_author_id ON characters (author_id);
+CREATE INDEX idx_characters_char_type ON characters (char_type);
+CREATE INDEX idx_characters_name ON characters (name);
+CREATE INDEX idx_characters_related_heros ON characters (related_heros);
+CREATE INDEX idx_characters_related_villains ON characters (related_villains);
+CREATE INDEX idx_characters_is_active ON characters (is_active);
+CREATE INDEX idx_characters_is_deleted ON characters (is_deleted);
+CREATE INDEX idx_characters_created_at ON characters (created_at);
+CREATE INDEX idx_characters_updated_at ON characters (updated_at);
 
