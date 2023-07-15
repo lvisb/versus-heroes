f44805264859db7b65744bbe7a6f5e0af4f1f49f
Luis <luis.flashdev@gmail.com>
2023-07-15 12:32:57 -0300

    refactor(backend.prompts): adjustments

--- backend/src/chatgpt/prompts/char.prompt.ts
+++ backend/src/chatgpt/prompts/char.prompt.ts
@@ -10,12 +10,12 @@ export const charMainNamePrompt = (namesList: string[]) =>
 export const charSummaryPrompt = (charName: string) => `Who is ${charName}? Return a JSON containing the summary, and also char_type: If villain or a hero.`
 
 export const charHistoryPrompt = (charName: string) =>
-  `Make sure you return only text and ou should not write in bullet points, enumerate items, or create topics. Build a narrative based on the following information: Tell the story of ${charName}, start with their personality, then their level of intelligence, followed by their appearance, their abilities, their specialty, their preferences, their fears, and finally their good or bad deeds. Convert the response to text and return the history.`
+  `Make sure you return only text and should not write in bullet points, enumerate items, or create topics. Build a narrative based on the following information: Tell the story of ${charName}, start with their personality, then their level of intelligence, followed by their appearance, their abilities, their specialty, their preferences, their fears, and finally their good or bad deeds. Convert the response to text and return the history.`
 
-export const charAppearance = (charName: string) => `You should not write in bullet points or enumerate items, return only text not JSON. Utilize all your existing information to describe the appearance of the character ${charName}, preferably in a meticulous manner.`
+export const charAppearance = (charName: string) => `You should not write in bullet points or enumerate items, return only text not JSON, and make sure to not maxout 1000 characters. Utilize all your existing information to describe the appearance of the character ${charName}, preferably in a meticulous manner.`
 
-export const charStrenghtsPrompt = `Considering the character's entire history, list up to 5 strenghts of the character, without explaining why, and return as array of string in javascript without line break.`
+export const charStrenghtsPrompt = `Make sure you return only text and should not write in bullet points, enumerate items, or create topics. Considering the character's entire history, list up to 5 strenghts of the character, without explaining why. The JSON must contain the strenghts property.`
 
-export const charWeaknessesPrompt = `Considering the character's entire history, list up to 5 weaknesses of the character, without explaining why, and return as array of string in javascript without line break.`
+export const charWeaknessesPrompt = `Make sure you return only text and should not write in bullet points, enumerate items, or create topics. Considering the character's entire history, list up to 5 weaknesses of the character, without explaining why. The JSON must contain the weaknesses property.`
 
 export const charAttributesPrompt = `Considering the entire character's history, assign values from 1 to 5 for each of the attributes below, without explaining why, and return as a javascript serialized json: strength,intelligence,speed,defense,vitality,agility,technique,mobility,endurance,evasion`
