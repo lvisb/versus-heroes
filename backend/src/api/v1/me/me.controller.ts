6308ab7919598a820a4bdafcee79c09ef0eba5ab
Luis <luis.flashdev@gmail.com>
2023-07-18 12:42:45 -0300

    feat(backend.me): get current user info

--- backend/src/api/v1/me/me.controller.ts
+++ backend/src/api/v1/me/me.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Req, UseGuards } from '@nestjs/common'
+import { TokenGuard } from '../auth/guards/token.guard.js'
+import { SignedInRequest } from '#common/types/signed-in-request.type.js'
+import { HttpResponse } from '#common/utils/http-response.util.js'
 
+@UseGuards(TokenGuard)
 @Controller('api/v1/me')
-export class MeController {}
+export class MeController {
+  @Get()
+  async me(@Req() req: SignedInRequest) {
+    return HttpResponse.createBody({
+      user: {
+        id: req.user.sub,
+        email: req.user.email,
+      },
+    })
+  }
+}
--- backend/src/api/v1/me/me.service.ts
+++ backend/src/api/v1/me/me.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class MeService {}
+export class MeService {
+  constructor() {}
+}
