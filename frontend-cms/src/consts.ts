f911a13e0528cdabf8d6ec236c2007b7a4405856
Luis <luis.flashdev@gmail.com>
2023-07-20 14:03:42 -0300

    fix(frontend.admin): temporary fix por the api url

--- frontend-cms/src/consts.ts
+++ frontend-cms/src/consts.ts
@@ -1,5 +1,6 @@
 export namespace api {
-  export const baseUrl = import.meta.env.VITE_API_BASE_URL as string;
+  export const baseUrl = 'http://3.14.15.191:8001/api/v1'
+  // import.meta.env.VITE_API_BASE_URL as string;
 }
 
 export namespace supabase {
