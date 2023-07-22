6c4abe09262ccc9dd86d00828b676b69341495aa
Luis <luis.flashdev@gmail.com>
2023-07-22 18:20:45 -0300

    feat(frontend.admin): supabase assets url from env file

--- frontend-cms/src/consts.ts
+++ frontend-cms/src/consts.ts
@@ -1,11 +1,9 @@
 export namespace api {
-  export const baseUrl = 'http://3.14.15.191:8001/api/v1'
-  // import.meta.env.VITE_API_BASE_URL as string;
+  export const baseUrl = import.meta.env.VITE_API_BASE_URL as string;
 }
 
 export namespace supabase {
-  export const assetsUrl =
-    "https://apcuctqgazqqjobpdhax.supabase.co/storage/v1/object/public";
+  export const assetsUrl = import.meta.env.VITE_SUPABASE_ASSETS_URL as string;
 
   export const charAssetsUrl = `${assetsUrl}/characters`;
 }
